c6c0f9700985b8fe1cb5b3c91b58c6e892360a7a
bafir24 <bafirgenius@gmail.com>
2023-02-09 17:00:42 +0700

    install redux

---
+++ backend/products.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        id: 1,
+        name: "Iphone 12 Pro ",
+        desc: "6.1-inch display ",
+        price: 999,
+        image: "https://res.cloudinary.com/dyhbjdod5/image/upload/v1675925023/rdshop/iphone12pro_oqidcq.jpg",
+    },
+    {
+        id: 2,
+        name: "Iphone 12 ",
+        desc: "5.1-inch display ",
+        price: 899,
+        image: "https://res.cloudinary.com/dyhbjdod5/image/upload/v1675925018/rdshop/iphone12_yg6wou.jpg",
+    },
+    {
+        id: 3,
+        name: "galaxy S  ",
+        desc: "5.1-inch display ",
+        price: 699,
+        image: "https://res.cloudinary.com/dyhbjdod5/image/upload/v1675925013/rdshop/galaxyS_ikomrn.png",
+    },
+];
+
+module.exports = products;
---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Cart from "./components/Cart";
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/" element={<Home />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
