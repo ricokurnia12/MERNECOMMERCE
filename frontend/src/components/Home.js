593f54208eae7c45bc5ea0750ebac41241e891f7
bafir24 <bafirgenius@gmail.com>
2023-02-10 16:02:50 +0700

    adding products

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import { useGetAllProductsQuery } from "../features/productsApi";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+    const { data, error, isLoading } = useGetAllProductsQuery();
+    console.log(data, "ini data");
+    return (
+        <div className="home-container">
+            {isLoading ? (
+                <p>Loading.,.,.,.,</p>
+            ) : error ? (
+                <p>An error occured </p>
+            ) : (
+                <>
+                    <h2>New Arrival</h2>
+                    <div className="products">
+                        {data?.map((products) => (
+                            <div
+                                key={products.id}
+                                className="products"
+                            >
+                                <h3>{products.name}</h3>
+                                <img
+                                    src={products.image}
+                                    alt={products.name}
+                                />
+                                <div className="details">
+                                    <span>{products.desc}</span>
+                                    <span className="price">
+                                        ${products.price}
+                                    </span>
+                                    <button>Add to cart</button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </>
+            )}
+        </div>
+    );
+};
 
-export default Home
+export default Home;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import { configureStore} from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import productsReducer, {
+    productsFetch,
+} from "./features/productsSlice";
+import { productsApi } from "./features/productsApi";
+
+const store = configureStore({
+    reducer: {
+        products: productsReducer,
+        [productsApi.reducerPath]: productsApi.reducer, 
+    },
+    middleware: (getDefaultMiddleware)=>{
+        return getDefaultMiddleware().concat(productsApi.middleware);
+    },
+});
+
+store.dispatch(productsFetch());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
